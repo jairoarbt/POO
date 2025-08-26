f511150ca16c91202d3b97ca939289abc521a0a2
jairoarbt <jairo.arboledat@campusucc.edu.co>
2025-08-26 14:59:34 +0000

    Avances en clase

---
+++ Condicionales.java
@@ -0,0 +1,38 @@
+public class Condicionales{
+
+    public static void main (String []args){
+
+
+        int edad = 18;
+         
+        if (edad >= 18){
+            System.out.println("Mayor edad");
+        }else{
+            System.out.println( "No es mayor de edad");
+        }
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ Ejercicio185.java
@@ -0,0 +1,106 @@
+public class Ejercicio185 {
+
+
+    public static void main(String[] args) {
+
+
+        //ejercicio 4.1
+        //Punto a
+
+        System.out.println("Punto a");
+
+        int angulo = 90;
+
+        if (angulo == 90){
+
+            System.out.println("El angulo es un angulo recto");
+
+        }else{
+            System.out.println("El angulono es angulo recto");
+        }
+
+
+        System.out.println("-----------------------------------");
+        //Punto b
+        System.out.println("Punto b");
+
+        int grados = 90;
+
+        if (grados >100){
+
+            System.out.println("Por encima del punto de ebullición del agua");
+
+        }else{
+            System.out.println("Por debajo del puno de ebullicion del agua");
+        }
+
+
+        System.out.println("-----------------------------------");
+        //Punto c
+        System.out.println("Punto c");
+
+        int negativo = 0;
+        int positivo = 0;
+
+        int numero = -2;
+
+
+        
+
+        if(numero >= 0){
+            positivo =+ numero;
+        System.out.println("Positivos es :" + positivo);
+        }else{
+            negativo =+ numero;
+        System.out.println("Negativos es:" + negativo);
+        }
+
+        System.out.println("-----------------------------------");
+        //Punto d
+        System.out.println("Punto d");
+
+        int x = 10;
+        int y = 8;
+        int z = 12; 
+        if (x > y && z < 20 ){
+            System.out.println("El valor de P es 10");
+        
+        }else{
+            System.out.println("No se le puede dar vaor a P porque no cumple las condiciones dadas");
+
+        }
+ 
+        
+            
+        
+
+
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+        
+
+
+
+        
+
+
+
+
+
+
+
+    }
+    
+
