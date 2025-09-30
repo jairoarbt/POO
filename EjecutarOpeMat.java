f9e158ae44b4d765dd4fa8e894c0d2144217bae2
jairoarbt <jairo.arboledat@campusucc.edu.co>
2025-09-30 14:33:29 +0000

    avance clase 9

---
+++ EjecutarOpeMat.java
@@ -0,0 +1,29 @@
+public class EjecutarOpeMat {
+
+    public static void main(String[] args) {
+        //crear objeto 
+        OperacionesMatematicas objOpe = new OperacionesMatematicas(5, 6);
+
+        //acceder el objeto
+        
+        System.out.println("la suma es " + objOpe.sumar());
+
+        //acceder al metodo restar
+         System.out.println("la resta es " + objOpe.restar());
+         //acceder al metodo dividir
+
+         System.out.println("la dvision es: " + objOpe.dividir());
+         //acceder al metodo multplicacion
+
+         System.out.println("la multicacion es: " + objOpe.multiplicar());
+
+
+
+
+
+
+
+    }
+
+    
+}
---
+++ OperacionesMatematicas.java
@@ -0,0 +1,50 @@
+public class OperacionesMatematicas {
+
+    //Atributos
+
+    private int a;
+    private int b;
+
+    //Constructor permite inicializar la clase
+    public OperacionesMatematicas (int a , int b){
+
+        this.a = a;
+        this.b = b;
+    }
+
+
+
+    //metodos
+    public int sumar(){
+        return a + b;
+
+    }
+    public int restar(){
+        return a - b;
+
+
+    }
+    public double dividir(){
+        double resultado;
+        if ( b == 0){
+        resultado = 0;
+        }else{
+        //(double) significa hacer una converion de entero a real
+        resultado = (double) a / (double) b;
+        }
+        
+        return resultado;
+    }
+    
+    public int multiplicar(){
+        return a * b;
+    
+
+
+    }
+
+
+    
+
+    
+}
